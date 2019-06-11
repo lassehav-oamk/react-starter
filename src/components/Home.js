3c774fad29991de37d1f18fb0140b9e7cef4a015
Lasse Haverinen <lasse.haverinen@oamk.fi>
2019-06-11 15:00:09 +0300

    Added switch component to home view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Header from './Header';
 import View1 from './View1';
 import View2 from './View2';
@@ -9,18 +9,15 @@ export default class Home extends Component {
         const { match } = this.props;
 
         const content = <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi risus, tincidunt eget porttitor quis, interdum vel orci. Nam pellentesque nunc sed purus semper fringilla. Sed nec nisl luctus, malesuada tellus et, luctus nunc. Ut diam sem, tincidunt gravida mollis sed, suscipit in mauris. </div>;
-        const homeContent = match.isExact ? content : null;
-
-        console.log(match);
 
         return (
             <div>
-                <Header />            
-                { homeContent }
-                <div>                                         
+                <Header />
+                <Switch>
                     <Route path={`${match.url}/view1`} exact component={View1} />
                     <Route path={`${match.url}/view2`} exact component={View2} />                    
-                </div>
+                    <Route render={() => <div>{content}</div>} />
+                </Switch>
             </div>
         )
     }
